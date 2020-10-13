8ca9b367b72dc285cf0a4b2b13dd4791b803c31d
Luke Norris <luke.mjn@gmail.com>
2020-10-13 22:12:01 +0200

    changed Server from require syntax to muodules

--- server.js
+++ server.js
@@ -3,20 +3,12 @@ import colors from 'colors'
 import dotenv from 'dotenv'
 import fs from 'fs'
 
-// const express = require('express')
-// const colors = require('colors')
-// const dotenv = require('dotenv')
-// const fs = require('fs')
-
-
-
 dotenv.config()
 
 
 const secretKey = process.env.STRIPE_SECRET_KEY
 const publicKey = process.env.STRIPE_PUBLIC_KEY
 
-// const stripe = require('stripe')(secretKey)
 import Stripe from 'stripe'
 var stripe = Stripe(secretKey)
 
@@ -76,65 +68,3 @@ app.use(express.static('public'))
 
 app.listen(3000, ()=>console.log('Server running on Port: 3000'.yellow.bold))
 
-// const dotenv = require('dotenv')
-  
-//   const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-//   const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
-  
-//   const express = require('express')
-//   const app = express()
-//   const fs = require('fs')
-//   const colors = require('colors')
-//   const stripe = require('stripe')(stripeSecretKey)
-
-//   dotenv.config()
-
-  
-//   app.set('view engine', 'ejs')
-//   app.use(express.json())
-//   app.use(express.static('public'))
-  
-//   app.get('/store', function(req, res) {
-//     fs.readFile('items.json', function(error, data) {
-//       if (error) {
-//         res.status(500).end()
-//       } else {
-//         res.render('store.ejs', {
-//           stripePublicKey: stripePublicKey,
-//           items: JSON.parse(data)
-//         })
-//       }
-//     })
-//   })
-  
-//   app.post('/purchase', function(req, res) {
-//     fs.readFile('items.json', function(error, data) {
-//       if (error) {
-//         res.status(500).end()
-//       } else {
-//         const itemsJson = JSON.parse(data)
-//         const itemsArray = itemsJson.music.concat(itemsJson.merch)
-//         let total = 0
-//         req.body.items.forEach(function(item) {
-//           const itemJson = itemsArray.find(function(i) {
-//             return i.id == item.id
-//           })
-//           total = total + itemJson.price * item.quantity
-//         })
-  
-//         stripe.charges.create({
-//           amount: total,
-//           source: req.body.stripeTokenId,
-//           currency: 'usd'
-//         }).then(function() {
-//           console.log('Charge Successful')
-//           res.json({ message: 'Successfully purchased items' })
-//         }).catch(function() {
-//           console.log('Charge Fail')
-//           res.status(500).end()
-//         })
-//       }
-//     })
-//   })
-  
-//   app.listen(3000, ()=>console.log('Server running on Port: 3000'.yellow.bold))
